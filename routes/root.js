46b06dead0c78f607fcfad30399c0b51a454e596
AlexCheese <alexip013@gmail.com>
2025-04-21 13:17:15 -0400

    create base meeting page

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,16 @@ import express from "express";
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-    res.render('home', {title: 'Book.it Meeting Scheduler'});
+    res.render("home", { title: "Book.it Meeting Scheduler" });
+});
+
+router.route("/:meetingId").get(async (req, res) => {
+    res.render("viewMeeting", {
+        title: "Test Meeting",
+        canEdit: true,
+        meetingId: "abc123",
+        meetingDescription: "The big meetup",
+    });
 });
 
 export default router;
