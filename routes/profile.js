7776f4dabe3af6c21395bcdf2cd436b3317c6a6e
AlexCheese <alexip013@gmail.com>
2025-04-21 11:31:29 -0400

    create base profile page

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,12 @@ import express from "express";
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-    res.json("implement me");
+    res.render('profile', {
+        title: 'My Profile',
+        canEdit: true,
+        fullName: "Alex Prikockis",
+        pfpUrl: 'https://files.alexcheese.com/u/AWmGOQ.png'
+    });
 });
 
 export default router;
