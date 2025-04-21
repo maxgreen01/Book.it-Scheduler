32136bd89af0067944d07bf126165ab7ab79a24b
AlexCheese <alexip013@gmail.com>
2025-04-21 18:55:19 -0400

    create base meeting creation page

--- routes/create.js
+++ routes/create.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-    res.json("implement me");
+    res.render("createMeeting", { title: "Create New Meeting" });
 });
 
 export default router;
