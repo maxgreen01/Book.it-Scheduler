1f9f68d2230281150cb21a4dc884165fbdf7b8b5
blee121 <blee12@stevens.edu>
2025-04-30 10:38:15 -0400

    Modify ' / ' route for handlebars comments section support

--- routes/root.js
+++ routes/root.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { getAllComments, getMeetingComments } from "../data/comments.js";
+import { renderError } from "../utils/routeUtils.js";
 
 const router = express.Router();
 
@@ -7,12 +9,21 @@ router.route("/").get(async (req, res) => {
 });
 
 router.route("/:meetingId").get(async (req, res) => {
-    res.render("viewMeeting", {
-        title: "Test Meeting",
-        canEdit: true,
-        meetingId: "abc123",
-        meetingDescription: "The big meetup",
-    });
+    //NOTE: dont do validation for now so we can use test Ids
+    // can render a 400 with route-level validation once we do
+    const meetingId = req.params.meetingId;
+
+    try {
+        //plug meeting comments into page from db
+        const comments = await getAllComments();
+        // const comments = await getMeetingComments(meetingId)
+        res.render("viewMeeting", {
+            title: "Test Meeting",
+            comments: comments,
+        });
+    } catch (e) {
+        return renderError(res, 404, e.message);
+    }
 });
 
 router.route("/:meetingId/edit").get(async (req, res) => {
