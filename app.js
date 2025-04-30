41e0600d312c8ea9e3f5c995dd838911dcbf3702
AlexCheese <alexip013@gmail.com>
2025-04-30 14:46:20 -0400

    add logging middleware and reorder middleware

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ app.use("/public", express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Handlebars setup
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// File uploading
+const profilePictureUpload = fileUpload({
+    limits: {
+        fileSize: 5000000,
+    },
+    abortOnLimit: true,
+    responseOnLimit: "Profile picture size cannot exceed 5 megabytes!",
+});
+
+// Session
 app.use(
     session({
         name: "AuthenticationState",
@@ -28,23 +42,17 @@ app.use(
     })
 );
 
-const profilePictureUpload = fileUpload({
-    limits: {
-        fileSize: 5000000,
-    },
-    abortOnLimit: true,
-    responseOnLimit: "Profile picture size cannot exceed 5 megabytes!",
-});
-app.post("/login/signup", profilePictureUpload);
-app.patch("/profile", profilePictureUpload);
-
-// Handlebars setup
-app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 //
 // ================ custom middlewares ================
 //
+
+// Logging middleware
+app.use("/", async (req, res, next) => {
+    const timestamp = new Date().toUTCString();
+    const auth = req.session.user ? req.session.user._id : "?";
+    console.log(`[${timestamp}]: (${auth}) ${req.method} ${req.path} ${req.body ? JSON.stringify(req.body) : ""}`);
+    next();
+});
 
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
     // If the user posts to the server with a property called _method, rewrite the request's method to be that method
@@ -57,14 +65,23 @@ const rewriteUnsupportedBrowserMethods = (req, res, next) => {
 };
 app.use(rewriteUnsupportedBrowserMethods);
 
+// Fallback error handler
+app.use((err, req, res, next) => {
+    console.error("route error:");
+    console.error(err);
+    res.sendStatus(500);
+});
+
+//
+// ================ routing ================
+//
+
+app.post("/login/signup", profilePictureUpload);
+app.patch("/profile", profilePictureUpload);
+
 configRoutes(app);
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:3000");
-});
-
-app.use((err, req, res, next) => {
-    console.error("route error:");
-    console.error(err);
 });
