06947398f8c274528b539c87a78b9b7bcd7b8c04
AlexCheese <alexip013@gmail.com>
2025-05-02 23:27:16 -0400

    implement client-side validation for auth forms

---
+++ public/js/validateAuthForms.js
@@ -0,0 +1,47 @@
+import { validateAndTrimString, validateUserId } from "./clientValidation.js";
+import { createUserDocument } from "./documentCreation.js";
+
+function setError(err) {
+    const errNode = document.getElementById("error");
+    errNode.innerHTML = `${err.message}`;
+}
+
+function validateSignup(event) {
+    const firstName = document.getElementById("firstNameInput").value;
+    const lastName = document.getElementById("lastNameInput").value;
+    const description = document.getElementById("descriptionInput").value;
+    const uid = document.getElementById("usernameInput").value;
+    const password = document.getElementById("passwordInput").value;
+    try {
+        const user = createUserDocument({
+            firstName,
+            lastName,
+            description,
+            uid,
+            password,
+            profilePicture: "0", // TODO pfp
+            availability: [0, 0, 0, 0, 0, 0, 0], // TODO availability
+        });
+    } catch (e) {
+        event.preventDefault();
+        setError(e);
+    }
+}
+
+function validateLogin(event) {
+    const uid = document.getElementById("usernameInput").value;
+    const password = document.getElementById("passwordInput").value;
+    try {
+        validateUserId(uid);
+        validateAndTrimString(password, "Password"); // TODO revise
+    } catch (e) {
+        event.preventDefault();
+        setError(e);
+    }
+}
+
+const signupForm = document.getElementById("signup");
+const loginForm = document.getElementById("login");
+
+if (signupForm) signupForm.addEventListener("submit", validateSignup);
+if (loginForm) loginForm.addEventListener("submit", validateLogin);
