0815c4930029400d2342a6fcd44c9a3242c7e898
Someone109 <praneeth.vanguru@gmail.com>
2025-05-08 21:19:08 -0400

    Removed Testing code from seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -154,48 +154,6 @@ async function seed() {
         commentIds.push(comment._id);
     }
 
-    // // TEST MEETING BRANCH
-
-    // // create comments for meeting test
-    // for (let i = 0; i < 8; i++) {
-    //     // randomly select a user and meeting for this comment
-    //     const uid = faker.helpers.arrayElement(userIds);
-    //     const meeting = "1234abcd1234abcd1234abcd"; //for the test meeting
-
-    //     const comment = await createComment({
-    //         uid: userId,
-    //         meetingId: meetingId,
-    //         body: faker.lorem.sentences({ min: 2, max: 4 }),
-    //     });
-
-    //     // lambda to show only a short bit of the comment
-    //     const preview = (str) => (str.length > 40 ? str.slice(0, 30) + "..." : str);
-
-    //     console.log(`Adding Comment ${i}: ${preview(comment.body)}`);
-    //     commentIds.push(comment._id);
-    // }
-
-    // // TEST MEETING BRANCH
-
-    // // create comments for meeting test
-    // for (let i = 0; i < 8; i++) {
-    //     // randomly select a user and meeting for this comment
-    //     const uid = faker.helpers.arrayElement(userIds);
-    //     const meeting = "1234abcd1234abcd1234abcd"; //for the test meeting
-
-    //     const comment = await commentFunctions.createComment({
-    //         uid: uid,
-    //         meetingId: meeting,
-    //         body: faker.lorem.sentences({ min: 2, max: 4 }),
-    //     });
-
-    //     // lambda to show only a short bit of the comment
-    //     const preview = (str) => (str.length > 40 ? str.slice(0, 30) + "..." : str);
-
-    //     console.log(`Adding Comment ${i}: ${preview(comment.body)}`);
-    //     commentIds.push(comment._id);
-    // }
-
     console.log("Finished seeding the database!");
 }
 
