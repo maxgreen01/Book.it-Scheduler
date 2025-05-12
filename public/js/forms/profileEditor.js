3bf0339d04d2e7c006edec667951526b2077932d
AlexCheese <alexip013@gmail.com>
2025-05-12 15:07:16 -0400

    client-side verification in profiler editor

--- public/js/forms/profileEditor.js
+++ public/js/forms/profileEditor.js
@@ -1,3 +1,12 @@
+import { validateImageFileType } from "../clientValidation.js";
+import { createUserDocument } from "../documentCreation.js";
+
+// Set error text in html element with id "error"
+function setError(err) {
+    const errNode = document.getElementById("error");
+    errNode.innerHTML = `${err.message}`;
+}
+
 function toggleEditor(num) {
     let info = document.getElementById("info");
     let editor = document.getElementById("editor");
@@ -12,8 +21,38 @@ async function deleteUser() {
     window.location.reload();
 }
 
+function validateProfile(event) {
+    const firstName = document.getElementById("firstNameInput").value;
+    const lastName = document.getElementById("lastNameInput").value;
+    const description = document.getElementById("descriptionInput").value;
+    const password = document.getElementById("passwordInput").value;
+    const confirmPassword = document.getElementById("confirmPasswordInput").value;
+    const files = document.getElementById("profilePictureInput").files;
+    try {
+        if (password != confirmPassword) throw new Error("Passwords do not match");
+        createUserDocument(
+            {
+                firstName,
+                lastName,
+                description,
+                password: password === "" ? undefined : password,
+            },
+            true
+        );
+        if (files && files[0]) {
+            let pfp = files[0];
+            validateImageFileType(pfp.name, "Profile Picture");
+            if (pfp.size > 5000000) throw new Error("Profile Picture must be under 5MB");
+        }
+    } catch (e) {
+        event.preventDefault();
+        setError(e);
+    }
+}
+
 document.getElementById("editProfile").addEventListener("click", () => toggleEditor(1));
 document.getElementById("cancelEdit").addEventListener("click", () => toggleEditor(0));
 document.getElementById("deleteUser").addEventListener("click", () => toggleEditor(2));
 document.getElementById("confirmDelete").addEventListener("click", deleteUser);
 document.getElementById("cancelDelete").addEventListener("click", () => toggleEditor(1));
+document.getElementById("editor").addEventListener("submit", validateProfile);
