ef214cf43831da5751b3c0e5f1456432ac6f14ec
Someone109 <praneeth.vanguru@gmail.com>
2025-05-13 23:10:20 -0400

    New Meetings to seed file

--- tasks/seed.js
+++ tasks/seed.js
@@ -8,6 +8,7 @@ import { Availability, WeeklyAvailability } from "../public/js/classes/availabil
 import { addResponseToMeeting, createMeeting, getMeetingById, replyToMeetingInvitation, setMeetingBooking, updateMeetingNote } from "../data/meetings.js";
 import { Response } from "../public/js/classes/responses.js";
 import { formatDateAsMinMaxString } from "../public/js/helpers.js";
+import { timeEnd } from "console";
 
 // define the seed procedure, which is called below
 async function seed() {
@@ -106,21 +107,21 @@ async function seed() {
                 description: faker.lorem.sentences(faker.number.int({ min: 1, max: 6 })),
                 duration: 2, // convert to a string input in hours
                 owner: "mgreen",
-                dateStart: "2025-06-22",
-                dateEnd: "2025-06-22",
+                dateStart: "2025-05-21",
+                dateEnd: "2025-05-21",
                 timeStart: 18,
                 timeEnd: 26,
             };
 
             const greatAddedMeeting = await createMeeting(greatMeetup, true);
 
-            const BrendanGreatResponse = new Response("Brendan123", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 5, 22))]);
-            const maxGreatResponse = new Response("mgreen", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 5, 22))]);
-            const alexGreatResponse = new Response("AlexCheese", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 5, 22))]);
+            const BrendanGreatResponse = new Response("Brendan123", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 4, 21))]);
+            const maxGreatResponse = new Response("mgreen", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 4, 21))]);
+            const alexGreatResponse = new Response("AlexCheese", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 4, 21))]);
             await addResponseToMeeting(greatAddedMeeting._id, BrendanGreatResponse);
             await addResponseToMeeting(greatAddedMeeting._id, maxGreatResponse);
             await addResponseToMeeting(greatAddedMeeting._id, alexGreatResponse);
-            await setMeetingBooking(greatAddedMeeting._id, 1, { timeStart: 18, timeEnd: 20, date: new Date(2025, 5, 22) });
+            await setMeetingBooking(greatAddedMeeting._id, 1, { timeStart: 18, timeEnd: 20, date: new Date(2025, 4, 21) });
 
             console.log(`Adding meeting #${i}: ${greatAddedMeeting.name}`);
 
@@ -129,20 +130,20 @@ async function seed() {
                 description: faker.lorem.sentences(faker.number.int({ min: 1, max: 6 })),
                 duration: 2, // convert to a string input in hours
                 owner: "mgreen",
-                dateStart: "2025-06-22",
-                dateEnd: "2025-06-22",
+                dateStart: "2025-05-21",
+                dateEnd: "2025-05-21",
                 timeStart: 18,
                 timeEnd: 26,
             };
 
             const bookItDevMeeting = await createMeeting(bookItDev, true);
-            const BrendanDevResponse = new Response("Brendan123", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 5, 22))]);
-            const maxDevResponse = new Response("mgreen", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 5, 22))]);
-            const alexDevResponse = new Response("AlexCheese", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 5, 22))]);
+            const BrendanDevResponse = new Response("Brendan123", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 4, 21))]);
+            const maxDevResponse = new Response("mgreen", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 4, 21))]);
+            const alexDevResponse = new Response("AlexCheese", [new Availability([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], new Date(2025, 4, 21))]);
             await addResponseToMeeting(bookItDevMeeting._id, BrendanDevResponse);
             await addResponseToMeeting(bookItDevMeeting._id, maxDevResponse);
             await addResponseToMeeting(bookItDevMeeting._id, alexDevResponse);
-            await setMeetingBooking(bookItDevMeeting._id, 1, { timeStart: 18, timeEnd: 20, date: new Date(2025, 5, 22) });
+            await setMeetingBooking(bookItDevMeeting._id, 1, { timeStart: 18, timeEnd: 20, date: new Date(2025, 4, 21) });
 
             //add 9
             const triviaClubEboard = {
@@ -150,13 +151,41 @@ async function seed() {
                 description: "The views and opinions expressed on this channel are those exclusive held by the STC, and are not indorsed or liked by Stevens in any way.",
                 duration: 2,
                 owner: "mgreen",
-                dateStart: "2025-06-22",
-                dateEnd: "2025-06-22",
+                dateStart: "2025-05-21",
+                dateEnd: "2025-05-21",
                 timeStart: 18,
                 timeEnd: 26,
             };
 
             await createMeeting(triviaClubEboard, true);
+
+            const ajaxCelebration = {
+                name: "Celebration of AJAX",
+                description: "Gather to celebrate",
+                duration: 2,
+                owner: "mgreen",
+                dateStart: "2025-05-21",
+                dateEnd: "2025-05-21",
+                timeStart: 18,
+                timeEnd: 26,
+            };
+
+            const ajaxMeeting = await createMeeting(ajaxCelebration, true);
+            await setMeetingBooking(ajaxMeeting._id, -1, null);
+
+            const maxbirthday = {
+                name: "Max's Birthday",
+                description: "It's his birthday guys!",
+                duration: 2,
+                owner: "mgreen",
+                dateStart: "2025-03-21",
+                dateEnd: "2025-03-21",
+                timeStart: 18,
+                timeEnd: 26,
+            };
+
+            const maxbirthdayMeeting = await createMeeting(maxbirthday, true);
+            await setMeetingBooking(maxbirthdayMeeting._id, 1, { timeStart: 18, timeEnd: 20, date: new Date(2025, 2, 21) });
 
             await replyToMeetingInvitation(bookItDevMeeting._id, "mgreen", 1);
             await replyToMeetingInvitation(bookItDevMeeting._id, "AlexCheese", 1);
